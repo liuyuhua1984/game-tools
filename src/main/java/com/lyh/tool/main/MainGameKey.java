c38ece4a05ed148cd81cf87938e8bb2104ca5c03
lyh@163.com <lyh@163.com>
2019-09-11 08:34:36 +0800

    gameMainGamekey修改

--- src/main/java/com/lyh/tool/main/MainGameKey.java
+++ src/main/java/com/lyh/tool/main/MainGameKey.java
@@ -1,26 +1,32 @@
 package com.lyh.tool.main;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+
+import java.util.Calendar;
 import java.util.Properties;
 
 /**
- * 后台账号生成类
+ * 设置生成游戏key值
  *
  * @author: root
- * @create: 2018-10-08 11:51
+ * @create: 2018-10-08 14:53
  **/
 
 public class MainGameKey {
-
-    /**res路径**/
+    /**
+     * res路径
+     **/
     public static String RES = "/res/";
 
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
 
         try {
             String userPath = System.getProperty("user.dir");
-            String filePath = "admin.properties";
+            String filePath = "game.properties";
 
             if (arg != null && arg.length > 0) {
                 userPath = arg[0];
@@ -30,42 +36,47 @@ public class MainGameKey {
             Properties properties = GameProperties.loadGameProperties(userPath+RES+filePath);
 
             String key = properties.getProperty("key");
-            String admin = properties.getProperty("admin");
-            String password = properties.getProperty("password");
+            int days =  Integer.parseInt(properties.getProperty("day"));
 
-            String md5String = Md5Encrypt.getMD5(admin+key);
-            String md5Password = Md5Encrypt.getMD5(key+password);
+            Calendar today = Calendar.getInstance();
+            today.add(Calendar.DAY_OF_YEAR,days);
+            today.set(Calendar.HOUR,0);
+            today.set(Calendar.MINUTE,0);
+            today.set(Calendar.SECOND,0);
+            today.set(Calendar.MILLISECOND,0);
+            String tm = ""+today.getTimeInMillis();
+            String md5Days = Md5Encrypt.getMD5(tm+key);
 
-            File file = new File(userPath+RES+"admin.dat");
-            //file.deleteOnExit();
-            //
-            //file.createNewFile();
+            File file = new File(userPath+RES+"game-key.dat");
 
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             //创建字节流缓冲区，加快写出速度
-          //  BufferedOutputStream bout=new BufferedOutputStream(fileOutputStream);
+            //  BufferedOutputStream bout=new BufferedOutputStream(fileOutputStream);
 
             //创建字符输出流对象
-           // BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(bout, "utf-8"));
-        //    FileWriter fileWriter = new FileWriter(userPath+RES+"admin.dat");
-            if (md5String != null) {
-                byte[] data = md5String.getBytes("utf-8");
-               // int index = 0;
+            // BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(bout, "utf-8"));
+            //    FileWriter fileWriter = new FileWriter(userPath+RES+"admin.dat");
+
+            if (tm != null){
+                byte[] data = tm.getBytes("utf-8");
+                // int index = 0;
                 for (int i = 0; i < data.length; i++){
                     data[i] ^=1;
                 }
 
                 fileOutputStream.write(data);
-
                 fileOutputStream.write("\r\n".getBytes("UTF-8"));
             }
 
-            if (md5Password != null){
-                byte [] data = md5Password.getBytes("UTF-8");
+            if (md5Days != null) {
+                byte[] data = md5Days.getBytes("utf-8");
+                // int index = 0;
                 for (int i = 0; i < data.length; i++){
                     data[i] ^=1;
                 }
+
                 fileOutputStream.write(data);
+
                 fileOutputStream.write("\r\n".getBytes("UTF-8"));
             }
 
@@ -78,32 +89,12 @@ public class MainGameKey {
                 fileOutputStream.write("\r\n".getBytes("UTF-8"));
             }
 
-            if (admin != null){
-                byte [] data = admin.getBytes("UTF-8");
-                for (int i = 0; i < data.length; i++){
-                    data[i] ^=1;
-                }
-                fileOutputStream.write(data);
-                fileOutputStream.write("\r\n".getBytes("UTF-8"));
-            }
-
-            if (password != null){
-                byte [] data = password.getBytes("UTF-8");
-                for (int i = 0; i < data.length; i++){
-                    data[i] ^=1;
-                }
-                fileOutputStream.write(data);
-                fileOutputStream.write("\r\n".getBytes("UTF-8"));
-            }
-
-
             fileOutputStream.flush();
             fileOutputStream.close();
 
-
-        System.out.println("保存admin成功");
         }catch (Exception e){
             e.printStackTrace();
+            System.exit(0);
         }
     }
 }
